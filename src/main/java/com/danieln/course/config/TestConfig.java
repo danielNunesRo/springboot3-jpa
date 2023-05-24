c0a7c53047ed74fc709e4b3bac75b28fa6100891
Daniel Nunes <danielnunespk@gmail.com>
2023-05-24 14:48:01 -0300

    Entity Category

--- src/main/java/com/danieln/course/config/TestConfig.java
+++ src/main/java/com/danieln/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.danieln.course.entities.Category;
 import com.danieln.course.entities.Order;
 import com.danieln.course.entities.User;
 import com.danieln.course.entities.enums.OrderStatus;
+import com.danieln.course.repositories.CategoryRepository;
 import com.danieln.course.repositories.OrderRepository;
 import com.danieln.course.repositories.UserRepository;
 
@@ -22,6 +24,9 @@ public class TestConfig implements CommandLineRunner {
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -33,7 +38,12 @@ public class TestConfig implements CommandLineRunner {
 		Order or2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order or3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(or1,or2,or3));
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 	}
 }
